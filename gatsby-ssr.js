b9bd6600c6d42b0145ac47da35cb05d9b06d2ef6
Negar Khajeddin <khajeddin@noreply.github.com>
2021-05-28 21:29:10 +0430

    Add Html/Body Attributes in gatsby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,1 +1,19 @@
 import './src/styles/tailwind.css';
+import React from 'react';
+
+const HtmlAttributes = {
+  lang: 'en',
+  dir: 'ltr',
+};
+
+const BodyAttributes = {
+  className: 'antialiased',
+};
+
+export const onRenderBody = (
+  { setHtmlAttributes, setBodyAttributes },
+  pluginOptions,
+) => {
+  setHtmlAttributes(HtmlAttributes);
+  setBodyAttributes(BodyAttributes);
+};
