ff8ac8f2b727ede2d671e4674cec50f4307974bf
Negar Khajeddin <khajeddin@noreply.github.com>
2021-06-06 17:56:53 +0430

    Add Gasby-plugin-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,11 @@ module.exports = {
   siteMetadata: {
     title: "Negar Khajeddin's Site",
   },
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp', // Needed for dynamic images
+  ],
 };
