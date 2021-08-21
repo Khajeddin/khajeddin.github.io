bd0d659ddf07fb1bd810179c9a69f10f7e8eed6b
Negar Khajeddin <khajeddin@noreply.github.com>
2021-08-21 23:21:08 +0430

    add basic style from format-html (blog posts)

--- src/assets/styles/globalStyles.ts
+++ src/assets/styles/globalStyles.ts
@@ -13,4 +13,50 @@ export default createGlobalStyle`
   p + p {
     ${tw`mt-3`};
   }
+  
+  .format-html {
+    ${tw`mt-2 px-2 text-gray-900`};
+
+    p {
+      ${tw`leading-relaxed pt-4 text-justify`}
+      text-indent: 1rem;
+    }
+
+    em {
+      ${tw`italic`};
+    }
+
+    strong {
+      ${tw`font-bold`};
+    }
+
+    h2 {
+      ${tw`font-medium text-xl pt-6`};
+    }
+
+    h3 {
+        ${tw`font-medium text-lg pt-5`};
+    }
+
+    h4 {
+        ${tw`font-medium text-base pt-5`};
+    }
+
+    a {
+        ${tw`underline`};
+    }
+
+    ul {
+        ${tw`list-disc pb-10 text-justify`};
+
+        text-indent: 0.5rem;
+    }
+
+    ol {
+        ${tw`list-decimal pb-10 text-justify`};
+
+        text-indent: 0.5rem;
+    }
+
+  }
 `;
